c6883c41d531d84d4b297d0a94451125fee3c383
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-05-27 17:08:34 +0530

    untested mac changer tool

--- mac/mac.py
+++ mac/mac.py
@@ -15,3 +15,15 @@ class mac_changer:
         mac_address=answer.group().split()[1]
         self.mac=mac_address
         return mac_address
+
+    def change_mac(self,iface,new_mac):
+        print("[+]  The current MAC address is", self.get_mac(iface))
+        output= subprocess.run(["ifconfig", iface, "down"], shell=False, capture_output=True)
+        print(output.stderr.decode())
+        output= subprocess.run(["ifconfig", iface,"hw", "ether", new_mac], shell=False, capture_output=True)
+        print(output.stderr.decode())
+        output= subprocess.run(["ifconfig", iface, "up"], shell=False, capture_output=True)
+        print(output.stderr.decode())
+
+        print("[+]  The new MAC address is: ", self.get_mac(iface))
+        return self.get_mac(iface)
--- mac/main.py
+++ mac/main.py
@@ -1,8 +1,11 @@
 from mac import mac_changer
 
 if __name__=="__main__":
-    print("Please enter your interface: ")
+    print("Please enter your interface: ", end="")
     interface=input()
+    print("please enter new mac address: ", end="")
+    newmac=input()
     m=mac_changer()
-    ans=m.get_mac(interface)
-    print(ans)
+    currmac=m.get_mac(interface)
+    changemac=m.change_mac(interface,newmac)
+    
